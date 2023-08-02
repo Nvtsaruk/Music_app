c52a596d2992d6373912d2aa5dd4993ea8d6645f
Nvtsaruk <nvtsaruk@gmail.com>
2023-08-02 09:32:32 +0300

    Initial Commit

---
+++ Music app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music app
+//
+//  Created by Tsaruk Nick on 2.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
