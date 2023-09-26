2319b391d2dba6422f02e77eb51b86e7ea1fc5f1
Nvtsaruk <nvtsaruk@gmail.com>
2023-09-26 21:11:42 +0300

    User profile changed to Decodable +  removed init

--- Music app/Classes/ModelLayer/Models/Network/UserProfile.swift
+++ Music app/Classes/ModelLayer/Models/Network/UserProfile.swift
@@ -1,24 +1,8 @@
-struct UserProfile: Codable {
+struct UserProfile: Decodable {
     let country: String
     let display_name: String
     let email: String
     let id: String
     let product: String
     let images: [ImageModel]
-    init(country: String, display_name: String, email: String, id: String, product: String, images: [ImageModel]) {
-        self.country = country
-        self.display_name = display_name
-        self.email = email
-        self.id = id
-        self.product = product
-        self.images = images
-    }
-    init() {
-        self.country = ""
-        self.display_name = ""
-        self.email = ""
-        self.id = ""
-        self.product = ""
-        self.images = []
-    }
 }
