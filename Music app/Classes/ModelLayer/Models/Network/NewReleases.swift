adb164796664bde5ef101551e87f77499cb5583c
Nvtsaruk <nvtsaruk@gmail.com>
2023-09-26 21:44:42 +0300

    fixed new releases model

--- Music app/Classes/ModelLayer/Models/Network/NewReleases.swift
+++ Music app/Classes/ModelLayer/Models/Network/NewReleases.swift
@@ -8,21 +8,14 @@ struct NewReleasesAlbums: Codable {
 }
 
 struct NewReleasesItem: Codable {
-    let album_type: String //Maybe enum
-    let total_tracks: Int
-//    let href: String?
-    let id: String?
-    let images: [ImageModel]?
-    let name: String?
-    let release_date: String?
-//    let type: String?
-    let genres: [String]?
-    let label: String?
+    let id: String
+    let images: [ImageModel]
+    let name: String
+    let release_date: String
     let artists: [NewReleasesArtist]
 }
 
 struct NewReleasesArtist: Codable {
-//    let href: String?
     let id: String?
     let name: String?
     let type: String?
