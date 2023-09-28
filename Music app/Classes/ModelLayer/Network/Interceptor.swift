3cb4e4d417b257fc8794a3d8c04af40faa958cf9
Nvtsaruk <nvtsaruk@gmail.com>
2023-09-28 13:35:19 +0300

    interceptor moved to separate file

---
+++ Music app/Classes/ModelLayer/Network/Interceptor.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Alamofire
+
+class APIRequestInterceptor: RequestInterceptor {
+    private var token: Token = Token(accessToken: "")
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        do {
+            let data = try CredentialStorageService().getPassword(for: KeychainConstants.accessToken.key)
+            self.token.accessToken = String(decoding: data ?? Data(), as: UTF8.self)
+        } catch {
+            print(error)
+        }
+        var urlRequest = urlRequest
+        urlRequest.headers.add(.authorization(bearerToken: token.accessToken))
+        completion(.success(urlRequest))
+    }
+    
+    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+        if let response = request.task?.response as? HTTPURLResponse, let responseError = error.asAFError, response.statusCode == 401 || response.statusCode == 400 || responseError.isResponseSerializationError {
+            LoginManager.shared.refreshToken()
+            completion(.retry)
+        } else {
+            completion(.doNotRetry)
+        }
+    }
+}
