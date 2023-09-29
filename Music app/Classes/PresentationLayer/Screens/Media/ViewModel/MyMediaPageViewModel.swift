33ea79b482a56ccf031691af694ccbf485a840b4
Nvtsaruk <nvtsaruk@gmail.com>
2023-09-29 13:51:32 +0300

    Fixed my media

--- Music app/Classes/PresentationLayer/Screens/Media/ViewModel/MyMediaPageViewModel.swift
+++ Music app/Classes/PresentationLayer/Screens/Media/ViewModel/MyMediaPageViewModel.swift
@@ -28,27 +28,26 @@ final class MyMediaPageViewModel: MyMediaPageViewModelProtocol {
     }
 
     private func getPlaylist(id: Int) {
-//        playlist = PlaylistModel(description: "", id: "", images: [ImageModel(url: "")], name: "", owner: PlaylistModelOwner(display_name: "", id: "", type: ""), tracks: PlaylistModelTracks(items: [PlaylistModelTracksItem(added_by: PlaylistModelTracksItemUser(id: "", type: ""), track: Track(album: TrackAlbum(album_type: "", artists: [TrackAlbumArtist(id: "", name: "", type: "")], id: "", images: [ImageModel(url: "")], name: "", release_date: "", type: ""), artists: [TrackAlbumArtist(id: "", name: "", type: "")], id: "", name: "", preview_url: "", type: ""))]), type: "")
-        playlist = PlaylistModel(description: "", id: "", images: [ImageModel(url: "")], name: "",owner: PlaylistModelOwner(display_name: "", id: "", type: ""),tracks: PlaylistModelTracks(), type: "")
+        playlist = PlaylistModel(description: "", id: "", images: [ImageModel(url: "")], name: "", owner: PlaylistModelOwner(display_name: "", id: "", type: ""), tracks: PlaylistModelTracks(items: [PlaylistModelTracksItem(added_at: "",added_by: PlaylistModelTracksItemUser(id: "", type: ""), track: Track(album: TrackAlbum(album_type: "", artists: [TrackAlbumArtist(id: "", name: "", type: "")], id: "", images: [ImageModel(url: "")], name: "", release_date: "", type: ""), artists: [TrackAlbumArtist(id: "", name: "", type: "")], id: "", name: "", preview_url: "", type: ""))]), type: "")
         playlist?.name = databasePlaylist[id].playlistName
         print(databasePlaylist[id].playlistName)
         guard let imageURL = databasePlaylist[id].tracks.first?.image else { return }
         playlist?.images = [ImageModel(url: imageURL)]
-//        var tracksArray: [PlaylistModelTracksItem] = []
-//        databasePlaylist[id].tracks.forEach{ track in
-//            let trackID = track.trackID
-//            let trackImage = track.image
-//            let trackName = track.trackName
-//            let artistName = track.artistName
-//            let trackAlbum = TrackAlbum(album_type: "", artists: [TrackAlbumArtist(id: "", name: artistName, type: "")], id: "", images: [ImageModel(url: trackImage)], name: "", release_date: "", type: "")
-//            let track  = Track(album: trackAlbum, artists: [TrackAlbumArtist(id: "", name: artistName, type: "artist")], id: "", name: trackName, preview_url: trackID, type: "track")
-//            let playlistModelTracksItemUser = PlaylistModelTracksItemUser(id: "", type: "")
-//            let playlistModelTracksItem = PlaylistModelTracksItem(added_by: playlistModelTracksItemUser, track: track)
-//            tracksArray.append(playlistModelTracksItem)
-//        }
+        var tracksArray: [PlaylistModelTracksItem] = []
+        databasePlaylist[id].tracks.forEach{ track in
+            let trackID = track.trackID
+            let trackImage = track.image
+            let trackName = track.trackName
+            let artistName = track.artistName
+            let trackAlbum = TrackAlbum(album_type: "", artists: [TrackAlbumArtist(id: "", name: artistName, type: "")], id: "", images: [ImageModel(url: trackImage)], name: "", release_date: "", type: "")
+            let track  = Track(album: trackAlbum, artists: [TrackAlbumArtist(id: "", name: artistName, type: "artist")], id: "", name: trackName, preview_url: trackID, type: "track")
+            let playlistModelTracksItemUser = PlaylistModelTracksItemUser(id: "", type: "")
+            let playlistModelTracksItem = PlaylistModelTracksItem(added_at: "",added_by: playlistModelTracksItemUser, track: track)
+            tracksArray.append(playlistModelTracksItem)
+        }
         
-//        let tracks = PlaylistModelTracks(items: tracksArray)
-//        playlist?.tracks = tracks
+        let tracks = PlaylistModelTracks(items: tracksArray)
+        playlist?.tracks = tracks
     }
 
     func showPlaylist(id: Int) {
