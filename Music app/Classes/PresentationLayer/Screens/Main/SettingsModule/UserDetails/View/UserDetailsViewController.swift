4ebf954100ace6f7ed6f10d3f71a9db2634fcd08
Nvtsaruk <nvtsaruk@gmail.com>
2023-09-30 10:54:53 +0300

    Userdetails refactored

--- Music app/Classes/PresentationLayer/Screens/Main/SettingsModule/UserDetails/View/UserDetailsViewController.swift
+++ Music app/Classes/PresentationLayer/Screens/Main/SettingsModule/UserDetails/View/UserDetailsViewController.swift
@@ -1,45 +1,33 @@
 import UIKit
 
-class Colors {
-    var gl:CAGradientLayer!
-    var colorTop: CGColor
-    var colorBottom: CGColor
+final class UserDetailsViewController: UIViewController {
+    //MARK: - IBOutlets
+    @IBOutlet private weak var avatarImage: UIImageView!
     
-    init(colorTop: CGColor, colorBottom: CGColor) {
-        self.colorTop = colorTop
-        self.colorBottom = colorBottom
-        self.gl = CAGradientLayer()
-        self.gl.colors = [colorTop, colorBottom]
-        self.gl.locations = [0.0, 0.5]
-    }
-}
-
-class UserDetailsViewController: UIViewController {
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var planLabel: UILabel!
+    @IBOutlet private weak var emailLabel: UILabel!
+    @IBOutlet private weak var countryLabel: UILabel!
+    @IBOutlet private weak var userIDLabel: UILabel!
     
-    @IBOutlet weak var avatarImage: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var planLabel: UILabel!
-    @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var countryLabel: UILabel!
-    @IBOutlet weak var userIDLabel: UILabel!
-    
+    //MARK: - Variables
     var viewModel: UserDetailsViewModelProtocol?
-    let symbolConfig = UIImage.SymbolConfiguration(pointSize: 30, weight: .medium, scale: .medium)
+    private let symbolConfig = UIImage.SymbolConfiguration(pointSize: 30, weight: .medium, scale: .medium)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         refresh()
     }
-    let colors = Colors(colorTop: UIColor.gray.cgColor, colorBottom: UIColor.black.cgColor)
-
-      func refresh() {
-          view.backgroundColor = UIColor.clear
-          let backgroundLayer = colors.gl
-          backgroundLayer?.frame = view.frame
-          view.layer.insertSublayer(backgroundLayer ?? CAGradientLayer(), at: 0)
-          }
-      
+    
+    private let colors = Colors(colorTop: UIColor.gray.cgColor, colorBottom: UIColor.black.cgColor)
+    
+    private func refresh() {
+        view.backgroundColor = UIColor.clear
+        let backgroundLayer = colors.gl
+        backgroundLayer?.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer ?? CAGradientLayer(), at: 0)
+    }
     
     private func setupUI() {
         navigationController?.navigationBar.topItem?.backButtonTitle = ""
@@ -47,22 +35,23 @@ class UserDetailsViewController: UIViewController {
         
         avatarImage.layer.cornerRadius = avatarImage.frame.height / 2
         nameLabel.text = viewModel?.currentUser?.display_name
+        
         let planLocalizedString = NSLocalizedString("plan", comment: "")
         let countryLocalizedString = NSLocalizedString("country", comment: "")
         let userIDLocalizedString = NSLocalizedString("userID", comment: "")
         let emailLocalizedString = NSLocalizedString("email", comment: "")
+        
         planLabel.text = planLocalizedString + " " + (viewModel?.currentUser?.product ?? "")
         countryLabel.text = countryLocalizedString + " " + (viewModel?.currentUser?.country ?? "")
         userIDLabel.text = userIDLocalizedString + " " + (viewModel?.currentUser?.id ?? "")
         emailLabel.text = emailLocalizedString + " " + (viewModel?.currentUser?.email ?? "")
+        
         if let url = viewModel?.currentUser?.images.first?.url {
             self.avatarImage.webImage(url: url)
         } else {
             self.avatarImage.image = UIImage(systemName: "person", withConfiguration: symbolConfig)
         }
     }
-    
-    
 }
 
 extension UserDetailsViewController: Storyboarded {
